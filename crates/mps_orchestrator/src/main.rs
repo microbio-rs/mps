79d9f0575481894c510c501dc7c59ad912d6906e
murilo ijanc <mbsd@m0x.ru>
2024-02-15 21:59:58 -0300

    feat: add more variables to render k8s templates

--- crates/mps_orchestrator/src/main.rs
+++ crates/mps_orchestrator/src/main.rs
@@ -1,9 +1,9 @@
 // Copyright (c) 2023 Murilo Ijanc' <mbsd@m0x.ru>
-// 
+//
 // Permission to use, copy, modify, and distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
 // copyright notice and this permission notice appear in all copies.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 // WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -13,13 +13,12 @@
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 use tera::{Context, Tera};
 use color_eyre::eyre::Result;
-use serde::Serialize;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
 
     // Crie uma instância Tera
-    let tera = Tera::new("templates/*.yml").expect("Falha ao carregar os templates");
+    let tera = Tera::new("templates/*.yml")?;
 
     // Crie um contexto de dados para o template
     let mut context = Context::new();
@@ -27,15 +26,21 @@ fn main() -> Result<()> {
     context.insert("port", &8081);
     context.insert("image", "gcr.io/google-samples/hello-app");
     context.insert("version", "1.0");
+    context.insert("namespace", "platform-engineering");
+    context.insert("domain", "info");
 
     // Renderize o template
-    let resultado = tera.render("deployment.yml", &context);
+    let names: Vec<&str> = tera.get_template_names().collect();
+    for tpl_name in names {
+        let resultado = tera.render(tpl_name, &context);
 
-    // Verifique se a renderização foi bem-sucedida
-    match resultado {
-        Ok(renderizado) => println!("Template renderizado:\n\n{}", renderizado),
-        Err(erro) => eprintln!("Erro ao renderizar o template: {:?}", erro),
-    };
+        // Verifique se a renderização foi bem-sucedida
+        match resultado {
+            Ok(renderizado) => println!("template renderizado:\n\n{}", renderizado),
+            Err(erro) => eprintln!("Erro ao renderizar o template: {:?}", erro),
+        };
+    }
+
 
     Ok(())
 }
