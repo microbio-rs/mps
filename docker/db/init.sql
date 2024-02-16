228ef19c4bab7cd4be31c439e8280f090c8bf868
murilo ijanc <mbsd@m0x.ru>
2024-02-16 16:36:24 -0300

    refact: add databases of crates

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -1,2 +1,14 @@
--- Criação de banco de dados mps
+-- Criação de banco de dados cloud
+CREATE DATABASE mps_cloud;
+
+-- Criação de banco de dados container
+CREATE DATABASE mps_container;
+
+-- Criação de banco de dados im
+CREATE DATABASE mps_im;
+
+-- Criação de banco de dados scm
+CREATE DATABASE mps_orchestrator;
+
+-- Criação de banco de dados orchestrator
 CREATE DATABASE mps_scm;
