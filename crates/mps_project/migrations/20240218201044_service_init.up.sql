f18182b8d59129500f13d0f4208556a3a190837d
murilo ijanc <mbsd@m0x.ru>
2024-02-23 15:14:43 -0300

    refactoring migrations

--- crates/mps_project/migrations/20240218201044_service_init.up.sql
+++ crates/mps_project/migrations/20240218201044_service_init.up.sql
@@ -1,7 +1,16 @@
-CREATE TABLE IF NOT EXISTS projects (
+CREATE TYPE service_kind AS ENUM (
+    'application',
+    'database'
+);
+
+CREATE TABLE IF NOT EXISTS services (
     id UUID PRIMARY KEY,
     user_id UUID NOT NULL,
+    project_id UUID NOT NULL,
+    environment_id UUID NOT NULL,
     name VARCHAR NOT NULL,
     description VARCHAR DEFAULT NULL,
+    kind service_kind NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL)
+
--- crates/mps_project/migrations/20240223170009_environment_init.up.sql
+++ crates/mps_project/migrations/20240223170009_environment_init.up.sql
@@ -1,7 +1,16 @@
-CREATE TABLE IF NOT EXISTS projects (
+CREATE TYPE environment_mode AS ENUM (
+    'development',
+    'production',
+    'staging'
+);
+
+CREATE TABLE IF NOT EXISTS environments (
     id UUID PRIMARY KEY,
     user_id UUID NOT NULL,
+    project_id UUID NOT NULL,
     name VARCHAR NOT NULL,
     description VARCHAR DEFAULT NULL,
+    mode environment_mode NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL)
+
